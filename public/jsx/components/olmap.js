951d6b257629220a802bbcb37c5e5725c15ac5d5
kjr63 <kaj.risberg@helsinkinet.fi>
2023-04-19 15:33:20 +0300

    Lisätty juttuja

--- public/jsx/components/olmap.js
+++ public/jsx/components/olmap.js
@@ -9,264 +9,265 @@ import {OSM, Vector as VectorSource} from 'ol/source.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import {fromLonLat} from 'ol/proj';
 
+const image = new CircleStyle({
+  radius: 8,
+  fill: new Fill({color:[250,128,114,0.5]}),
+  stroke: new Stroke({color: 'red', width: 3}),
+});
+
+const styles = {
+  'Point': new Style({
+	image: image,
+  }),
+  'LineString': new Style({
+	stroke: new Stroke({
+	  color: 'green',
+	  width: 1,
+	}),
+  }),
+  'MultiLineString': new Style({
+	stroke: new Stroke({
+	  color: 'green',
+	  width: 1,
+	}),
+  }),
+  'MultiPoint': new Style({
+	image: image,
+  }),
+  'MultiPolygon': new Style({
+	stroke: new Stroke({
+	  color: 'yellow',
+	  width: 1,
+	}),
+	fill: new Fill({
+	  color: 'rgba(255, 255, 0, 0.1)',
+	}),
+  }),
+  'Polygon': new Style({
+	stroke: new Stroke({
+	  color: 'blue',
+	  lineDash: [4],
+	  width: 3,
+	}),
+	fill: new Fill({
+	  color: 'rgba(0, 0, 255, 0.1)',
+	}),
+  }),
+  'GeometryCollection': new Style({
+	stroke: new Stroke({
+	  color: 'magenta',
+	  width: 2,
+	}),
+	fill: new Fill({
+	  color: 'magenta',
+	}),
+	image: new CircleStyle({
+	  radius: 10,
+	  fill: null,
+	  stroke: new Stroke({
+		color: 'magenta',
+	  }),
+	}),
+  }),
+  'Circle': new Style({
+	stroke: new Stroke({
+	  color: 'red',
+	  width: 2,
+	}),
+	fill: new Fill({
+	  color: 'rgba(255,0,0,0.2)',
+	}),
+  }),
+};
+
+const styleFunction = function (feature) {
+  return styles[feature.getGeometry().getType()];
+};
+
+const geojsonObject = {
+  'type': 'FeatureCollection',
+  'crs': {
+	'type': 'name',
+	'properties': {
+	  'name': 'EPSG:3857',
+	},
+  },
+  'features': [
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'Point',
+		'coordinates': [0, 0],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'LineString',
+		'coordinates': [
+		  [4e6, -2e6],
+		  [8e6, 2e6],
+		],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'LineString',
+		'coordinates': [
+		  [4e6, 2e6],
+		  [8e6, -2e6],
+		],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'Polygon',
+		'coordinates': [
+		  [
+			[-5e6, -1e6],
+			[-3e6, -1e6],
+			[-4e6, 1e6],
+			[-5e6, -1e6],
+		  ],
+		],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'MultiLineString',
+		'coordinates': [
+		  [
+			[-1e6, -7.5e5],
+			[-1e6, 7.5e5],
+		  ],
+		  [
+			[1e6, -7.5e5],
+			[1e6, 7.5e5],
+		  ],
+		  [
+			[-7.5e5, -1e6],
+			[7.5e5, -1e6],
+		  ],
+		  [
+			[-7.5e5, 1e6],
+			[7.5e5, 1e6],
+		  ],
+		],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'MultiPolygon',
+		'coordinates': [
+		  [
+			[
+			  [-5e6, 6e6],
+			  [-3e6, 6e6],
+			  [-3e6, 8e6],
+			  [-5e6, 8e6],
+			  [-5e6, 6e6],
+			],
+		  ],
+		  [
+			[
+			  [-2e6, 6e6],
+			  [0, 6e6],
+			  [0, 8e6],
+			  [-2e6, 8e6],
+			  [-2e6, 6e6],
+			],
+		  ],
+		  [
+			[
+			  [1e6, 6e6],
+			  [3e6, 6e6],
+			  [3e6, 8e6],
+			  [1e6, 8e6],
+			  [1e6, 6e6],
+			],
+		  ],
+		],
+	  },
+	},
+	{
+	  'type': 'Feature',
+	  'geometry': {
+		'type': 'GeometryCollection',
+		'geometries': [
+		  {
+			'type': 'LineString',
+			'coordinates': [
+			  [-5e6, -5e6],
+			  [0, -5e6],
+			],
+		  },
+		  {
+			'type': 'Point',
+			'coordinates': [4e6, -5e6],
+		  },
+		  {
+			'type': 'Polygon',
+			'coordinates': [
+			  [
+				[1e6, -6e6],
+				[3e6, -6e6],
+				[2e6, -4e6],
+				[1e6, -6e6],
+			  ],
+			],
+		  },
+		],
+	  },
+	},
+  ],
+};
+
+const geojsonTowns = {
+	'type': 'FeatureCollection',
+	'crs': {
+		'type': 'name',
+		'properties': {
+		  'name': 'EPSG:3857',
+		},
+	},
+	'features': [
+		{
+		  'type': 'Feature',
+		  'geometry': {
+			'type': 'Point',
+			'coordinates': fromLonLat([ 23.865888764, 61.167145977 ]),
+		  },
+		},
+	]			
+};
+
+const vectorSource = new VectorSource({
+  features: new GeoJSON().readFeatures(geojsonObject),
+});
+
+vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: styleFunction,
+});
+
+const testVectorLayer = new VectorLayer({
+	source: new VectorSource({
+		features: new GeoJSON().readFeatures(geojsonTowns),
+	}),
+	style: styleFunction,
+});
+
 export default class OlMap extends React.Component {
     constructor (props) {
         super(props);
     }
 	componentDidMount () {
-		const image = new CircleStyle({
-		  radius: 8,
-		  fill: null,
-		  stroke: new Stroke({color: 'red', width: 3}),
-		});
-
-		const styles = {
-		  'Point': new Style({
-			image: image,
-		  }),
-		  'LineString': new Style({
-			stroke: new Stroke({
-			  color: 'green',
-			  width: 1,
-			}),
-		  }),
-		  'MultiLineString': new Style({
-			stroke: new Stroke({
-			  color: 'green',
-			  width: 1,
-			}),
-		  }),
-		  'MultiPoint': new Style({
-			image: image,
-		  }),
-		  'MultiPolygon': new Style({
-			stroke: new Stroke({
-			  color: 'yellow',
-			  width: 1,
-			}),
-			fill: new Fill({
-			  color: 'rgba(255, 255, 0, 0.1)',
-			}),
-		  }),
-		  'Polygon': new Style({
-			stroke: new Stroke({
-			  color: 'blue',
-			  lineDash: [4],
-			  width: 3,
-			}),
-			fill: new Fill({
-			  color: 'rgba(0, 0, 255, 0.1)',
-			}),
-		  }),
-		  'GeometryCollection': new Style({
-			stroke: new Stroke({
-			  color: 'magenta',
-			  width: 2,
-			}),
-			fill: new Fill({
-			  color: 'magenta',
-			}),
-			image: new CircleStyle({
-			  radius: 10,
-			  fill: null,
-			  stroke: new Stroke({
-				color: 'magenta',
-			  }),
-			}),
-		  }),
-		  'Circle': new Style({
-			stroke: new Stroke({
-			  color: 'red',
-			  width: 2,
-			}),
-			fill: new Fill({
-			  color: 'rgba(255,0,0,0.2)',
-			}),
-		  }),
-		};
-
-		const styleFunction = function (feature) {
-		  return styles[feature.getGeometry().getType()];
-		};
-
-		const geojsonObject = {
-		  'type': 'FeatureCollection',
-		  'crs': {
-			'type': 'name',
-			'properties': {
-			  'name': 'EPSG:3857',
-			},
-		  },
-		  'features': [
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'Point',
-				'coordinates': [0, 0],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'LineString',
-				'coordinates': [
-				  [4e6, -2e6],
-				  [8e6, 2e6],
-				],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'LineString',
-				'coordinates': [
-				  [4e6, 2e6],
-				  [8e6, -2e6],
-				],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'Polygon',
-				'coordinates': [
-				  [
-					[-5e6, -1e6],
-					[-3e6, -1e6],
-					[-4e6, 1e6],
-					[-5e6, -1e6],
-				  ],
-				],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'MultiLineString',
-				'coordinates': [
-				  [
-					[-1e6, -7.5e5],
-					[-1e6, 7.5e5],
-				  ],
-				  [
-					[1e6, -7.5e5],
-					[1e6, 7.5e5],
-				  ],
-				  [
-					[-7.5e5, -1e6],
-					[7.5e5, -1e6],
-				  ],
-				  [
-					[-7.5e5, 1e6],
-					[7.5e5, 1e6],
-				  ],
-				],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'MultiPolygon',
-				'coordinates': [
-				  [
-					[
-					  [-5e6, 6e6],
-					  [-3e6, 6e6],
-					  [-3e6, 8e6],
-					  [-5e6, 8e6],
-					  [-5e6, 6e6],
-					],
-				  ],
-				  [
-					[
-					  [-2e6, 6e6],
-					  [0, 6e6],
-					  [0, 8e6],
-					  [-2e6, 8e6],
-					  [-2e6, 6e6],
-					],
-				  ],
-				  [
-					[
-					  [1e6, 6e6],
-					  [3e6, 6e6],
-					  [3e6, 8e6],
-					  [1e6, 8e6],
-					  [1e6, 6e6],
-					],
-				  ],
-				],
-			  },
-			},
-			{
-			  'type': 'Feature',
-			  'geometry': {
-				'type': 'GeometryCollection',
-				'geometries': [
-				  {
-					'type': 'LineString',
-					'coordinates': [
-					  [-5e6, -5e6],
-					  [0, -5e6],
-					],
-				  },
-				  {
-					'type': 'Point',
-					'coordinates': [4e6, -5e6],
-				  },
-				  {
-					'type': 'Polygon',
-					'coordinates': [
-					  [
-						[1e6, -6e6],
-						[3e6, -6e6],
-						[2e6, -4e6],
-						[1e6, -6e6],
-					  ],
-					],
-				  },
-				],
-			  },
-			},
-		  ],
-		};
-
-		const geojsonTowns = {
-			'type': 'FeatureCollection',
-			'crs': {
-				'type': 'name',
-				'properties': {
-				  'name': 'EPSG:3857',
-				},
-			},
-			'features': [
-				{
-				  'type': 'Feature',
-				  'geometry': {
-					'type': 'Point',
-					'coordinates': fromLonLat([ 23.865888764, 61.167145977 ]),
-				  },
-				},
-			]			
-		};
-
-		const vectorSource = new VectorSource({
-		  features: new GeoJSON().readFeatures(geojsonObject),
-		});
-
-		vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
-
-		const vectorLayer = new VectorLayer({
-		  source: vectorSource,
-		  style: styleFunction,
-		});
-		
-		const testVectorLayer = new VectorLayer({
-			source: new VectorSource({
-				features: new GeoJSON().readFeatures(geojsonTowns),
-			}),
-			style: styleFunction,
-		});
 
 		const map = new Map({
 		  layers: [
